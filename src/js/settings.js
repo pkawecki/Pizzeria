fa83cf4973f9c5d0382677fd79fba8a47e02eb40
Przemek Kawecki Toshiba <przemek.kawecki@gmail.com>
2021-09-16 00:25:34 +0200

    absolutely final commit

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,150 +1,150 @@
 export const select = {
-  templateOf: {
-    menuProduct: '#template-menu-product',
-    cartProduct: '#template-cart-product', // CODE ADDED
-    bookingWidget: '#template-booking-widget',
-  },
-  containerOf: {
-    menu: '#product-list',
-    cart: '#cart',
-    home : '.home-wrapper',
-    pages: '#pages',
-    booking: '.booking-wrapper',
-  },
-  all: {
-    menuProducts: '#product-list > .product',
-    menuProductsActive: '#product-list > .product.active',
-    formInputs: 'input, select',
-  },
-
-  menuProduct: {
-    clickable: '.product__header',
-    form: '.product__order',
-    priceElem: '.product__total-price .price',
-    imageWrapper: '.product__images',
-    amountWidget: '.widget-amount',
-    cartButton: '[href="#add-to-cart"]',
-  },
-
-  widgets: {
-    amount: {
-      input: 'input.amount', // CODE CHANGED
-      linkDecrease: 'a[href="#less"]',
-      linkIncrease: 'a[href="#more"]',
+    templateOf: {
+        menuProduct: '#template-menu-product',
+        cartProduct: '#template-cart-product', // CODE ADDED
+        bookingWidget: '#template-booking-widget',
     },
-    datePicker: {
-      wrapper: '.date-picker',
-      input: `input[name="date"]`,
+    containerOf: {
+        menu: '#product-list',
+        cart: '#cart',
+        home: '.home-wrapper',
+        pages: '#pages',
+        booking: '.booking-wrapper',
     },
-    hourPicker: {
-      wrapper: '.hour-picker',
-      input: 'input[type="range"]',
-      output: '.output',
+    all: {
+        menuProducts: '#product-list > .product',
+        menuProductsActive: '#product-list > .product.active',
+        formInputs: 'input, select',
     },
-  },
 
-  cart: {
-    productList: '.cart__order-summary',
-    toggleTrigger: '.cart__summary',
-    totalNumber: `.cart__total-number`,
-    totalPrice: '.cart__total-price strong, .cart__order-total .cart__order-price-sum strong',
-    subtotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
-    deliveryFee: '.cart__order-delivery .cart__order-price-sum strong',
-    form: '.cart__order',
-    formSubmit: '.cart__order [type="submit"]',
-    phone: '[name="phone"]',
-    address: '[name="address"]',
-  },
+    menuProduct: {
+        clickable: '.product__header',
+        form: '.product__order',
+        priceElem: '.product__total-price .price',
+        imageWrapper: '.product__images',
+        amountWidget: '.widget-amount',
+        cartButton: '[href="#add-to-cart"]',
+    },
 
-  cartProduct: {
-    amountWidget: '.widget-amount',
-    price: '.cart__product-price',
-    edit: '[href="#edit"]',
-    remove: '[href="#remove"]',
-  },
- 
-  booking: {
-    peopleAmount: '.people-amount',
-    hoursAmount: '.hours-amount',
-    tables: '.floor-plan .table',
-    floorPlan: '.floor-plan',
-    bookingForm: '.booking-form',
-    orderConfirmation : '.order-confirmation'
-  },
+    widgets: {
+        amount: {
+            input: 'input.amount', // CODE CHANGED
+            linkDecrease: 'a[href="#less"]',
+            linkIncrease: 'a[href="#more"]',
+        },
+        datePicker: {
+            wrapper: '.date-picker',
+            input: `input[name="date"]`,
+        },
+        hourPicker: {
+            wrapper: '.hour-picker',
+            input: 'input[type="range"]',
+            output: '.output',
+        },
+    },
 
-  nav: {
-    links: '.main-nav a',
-  },
+    cart: {
+        productList: '.cart__order-summary',
+        toggleTrigger: '.cart__summary',
+        totalNumber: `.cart__total-number`,
+        totalPrice: '.cart__total-price strong, .cart__order-total .cart__order-price-sum strong',
+        subtotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
+        deliveryFee: '.cart__order-delivery .cart__order-price-sum strong',
+        form: '.cart__order',
+        formSubmit: '.cart__order [type="submit"]',
+        phone: '[name="phone"]',
+        address: '[name="address"]',
+    },
+
+    cartProduct: {
+        amountWidget: '.widget-amount',
+        price: '.cart__product-price',
+        edit: '[href="#edit"]',
+        remove: '[href="#remove"]',
+    },
+
+    booking: {
+        peopleAmount: '.people-amount',
+        hoursAmount: '.hours-amount',
+        tables: '.floor-plan .table',
+        floorPlan: '.floor-plan',
+        bookingForm: '.booking-form',
+        orderConfirmation: '.order-confirmation'
+    },
+
+    nav: {
+        links: '.main-nav a',
+    },
 };
 
 export const classNames = {
-  menuProduct: {
-    wrapperActive: 'active',
-    imageVisible: 'active',
-  },
-  
-  cart: {
-    wrapperActive: 'active',
-  },
-  
-  booking: {
-    loading: 'loading',
-    tableBooked: 'booked',
-  },
+    menuProduct: {
+        wrapperActive: 'active',
+        imageVisible: 'active',
+    },
 
-  nav: {
-    active: 'active',
-  },
+    cart: {
+        wrapperActive: 'active',
+    },
 
-  pages: {
-    active: 'active',
-  }
+    booking: {
+        loading: 'loading',
+        tableBooked: 'booked',
+    },
+
+    nav: {
+        active: 'active',
+    },
+
+    pages: {
+        active: 'active',
+    }
 
 };
 
 
 export const settings = {
-  amountWidget: {
-    defaultValue: 1,
-    defaultMin: 1,
-    defaultMax: 9,
-  },
+    amountWidget: {
+        defaultValue: 1,
+        defaultMin: 1,
+        defaultMax: 9,
+    },
 
-  cart: {
-    defaultDeliveryFee: 20,
-  },
+    cart: {
+        defaultDeliveryFee: 20,
+    },
 
-  hours: {
-    open: 12,
-    close: 24,
-  },
+    hours: {
+        open: 12,
+        close: 24,
+    },
 
-  datePicker: {
-    maxDaysInFuture: 14,
-  },
-  
-  booking: {
-    tableIdAttribute: 'data-table',
-  },
+    datePicker: {
+        maxDaysInFuture: 14,
+    },
 
-  db: {
-    url: '//localhost:3131',
-    products: 'products',
-    orders: 'orders', //possible refactoring needed to *order*
-    booking: 'bookings', ////possible refactoring needed to *booking*
-    event: 'events',
-    dateStartParamKey: 'date_gte',
-    dateEndParamKey: 'date_lte',
-    notRepeatParam: 'repeat=false',
-    repeatParam: 'repeat_ne=false',
-  }
+    booking: {
+        tableIdAttribute: 'data-table',
+    },
+
+    db: {
+        url: '//' + window.location.hostname + (window.location.hostname == 'localhost' ? '3131' : ''),
+        products: 'products',
+        orders: 'orders', //possible refactoring needed to *order*
+        booking: 'bookings', ////possible refactoring needed to *booking*
+        event: 'events',
+        dateStartParamKey: 'date_gte',
+        dateEndParamKey: 'date_lte',
+        notRepeatParam: 'repeat=false',
+        repeatParam: 'repeat_ne=false',
+    }
 };
 
 export const templates = {
-  menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
-  
-  cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
+    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
 
-  bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML),
-  
+    cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
+
+    bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML),
+
 };
